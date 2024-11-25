72d21410a048b590f5165035e2401ca300b31d22
LEFEVRE <lefevrejulien@live.fr>
2024-11-25 10:04:25 +0100

    certifs

--- src/components/Certifications/Certif.js
+++ src/components/Certifications/Certif.js
@@ -1,17 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './Certif.css';
+import thm from "../../images/thm.jpg"
+import tp from "../../images/tp.jpg"
 
 const Certif = ({ onClose }) => {
+
+    const [enlargedImage, setEnlargedImage] = useState(null);
+    const [enlargedImagetp, setEnlargedImagetp] = useState(null);
+
+    const handleImageClick = (image) => {
+        setEnlargedImage(image); // Affiche l'image cliquée en grand
+        
+    };
+
+    const handleImageClicktp = (imagetp) => {
+         // Affiche l'image cliquée en grand
+        setEnlargedImagetp(imagetp);
+    };
+
+    const handleClose = () => {
+        setEnlargedImage(null);
+        setEnlargedImagetp(null); // Réinitialise l'image affichée
+    };
     const [images, setImages] = useState([]);
 
-    useEffect(() => {
+   {/** useEffect(() => {
         // Récupérer les images depuis l'API
         fetch('https://nimp-1.onrender.com/api/images')
             .then(response => response.json())
             .then(data => setImages(data))
             .catch(error => console.error('Erreur de récupération des images', error));
-    }, []);
+    }, []);*/} 
 
     return (
         <div className='certifs'>
@@ -20,16 +40,40 @@ const Certif = ({ onClose }) => {
                     <button onClick={onClose} className="close-button">X</button>
                     <h2>Mes certifications</h2>
                     <div className="images-container">
-                        {images.map(image => (
+                        {/*{images.map(image => (
                             <div key={image.id} className="image-item">
-                                <h3>{image.title}</h3>
-                                <img src={`https://jujuli1.github.io/nimp/images/${image.file_path}`} alt={image.title} />
+                                <h3>{image.title}</h3>*/}
+                                <div className='zoom'>
+                                <img src={tp} className='imgCertTp' alt='tp' onClick={() => handleImageClicktp(tp)}/>
+                                </div>
+                                <div className='zoom'>
+                                <img src={thm} className='imgCert' alt='thm' onClick={() => handleImageClick(thm)}/>
+                                </div>
+                                {enlargedImage && (
+                <div className="enlarged-container" onClick={handleClose}>
+                    <img src={enlargedImage} className="enlarged-img" alt="Certification agrandie" />
+                </div>
+                
+                
+            )}
+
+{enlargedImagetp && (
+                <div className="enlarged-containertp" onClick={handleClose}>
+                    <img src={enlargedImagetp} className="enlarged-imgtp" alt="Certification agrandie" />
+                </div>
+                
+                
+            )}
+                                
+                                
+                                
+                                {/*`https://jujuli1.github.io/nimp/images/${image.file_path}` */}
                             </div>
-                        ))}
+                        
                     </div>
                 </div>
             </div>
-        </div>
+        
     );
 };
 
