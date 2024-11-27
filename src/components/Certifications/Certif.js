9c2649069ae2c732de103be5abd99e37e27a4c9e
LEFEVRE <lefevrejulien@live.fr>
2024-11-27 20:51:05 +0100

    table

--- src/components/Certifications/Certif.js
+++ src/components/Certifications/Certif.js
@@ -43,7 +43,7 @@ const Certif = ({ onClose }) => {
                         {images.map(image => (
                             <div key={image.id} className="image-item">
                                 <h3>{image.title}</h3>
-                                 {image.file_path}</div>))}
+                                 </div>))}
                                {/* <div className='zoom'>
                                 <img src={tp} className='imgCertTp' alt='tp' onClick={() => handleImageClicktp(tp)}/>
                                 </div>
