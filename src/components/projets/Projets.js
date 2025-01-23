2c10b8989e409cd2f17ae2c7b215cf75b61743f6
LEFEVRE <lefevrejulien@live.fr>
2025-01-23 18:23:42 +0100

    projet2

--- src/components/projets/Projets.js
+++ src/components/projets/Projets.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Tanya from '../../images/sorciere.png'
 
 import './projets.css'
 
@@ -11,9 +12,17 @@ const Projets = ({onClose}) => {
             <div className="modal-overlay2">
                 <div className="modal-content2">
                     <button onClick={onClose} className="close-button">X</button>
+
+                    <div className='projets'>
+
                     <a href='https://jujuli1.github.io/weather/' >
                    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/The.Matrix.glmatrix.2.png/669px-The.Matrix.glmatrix.2.png' className='meteok' alt='matrix'/>     
                     </a>
+                    <a href='https://jujuli1.github.io/trajao/' >
+                   <img src={Tanya} className='meteok' alt='Tanya Rajaonarivelo dessin'/>     
+                    </a>
+
+                    </div>
                     
                     
                     </div>
